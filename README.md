a8084bb7a2c606d36506a7b22b249bd29c12922a
Julian Perelli <jperelli@gmail.com>
2022-07-18 02:32:31 +0200

    initial commit with test code

---
+++ README.md
@@ -0,0 +1,3 @@
+# vercel-python-gis
+
+A vercel python runtime that has GEOS, PROJ and GDAL installed so you can use the geospatial libraries of Django in vercel.
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { BuildOptions, Lambda } from "@vercel/build-utils";
+import execa from "execa";
+
+export const version = 1;
+
+export async function build({
+  workPath,
+  files,
+  entrypoint,
+  meta = {},
+  config = {},
+}: BuildOptions) {
+  console.log("installing haskell stack tool");
+
+  await execa.command("./build.sh", { shell: true });
+
+  const lambda = new Lambda({
+    files,
+    ...config,
+    handler: entrypoint,
+    runtime: "python3.9",
+  });
+
+  return {
+    output: lambda,
+  };
+}
