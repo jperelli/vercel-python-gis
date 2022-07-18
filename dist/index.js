5390e9bd861dc85e948ba6fb4b0f8357193ff8ce
Julian Perelli <jperelli@gmail.com>
2022-07-18 02:39:54 +0200

    add the js build

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.build = exports.version = void 0;
+const build_utils_1 = require("@vercel/build-utils");
+const execa_1 = __importDefault(require("execa"));
+exports.version = 1;
+function build({ workPath, files, entrypoint, meta = {}, config = {}, }) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("installing haskell stack tool");
+        yield execa_1.default.command("./build.sh", { shell: true });
+        const lambda = new build_utils_1.Lambda(Object.assign(Object.assign({ files }, config), { handler: entrypoint, runtime: "python3.9" }));
+        return {
+            output: lambda,
+        };
+    });
+}
+exports.build = build;
